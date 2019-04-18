b309150aa329b9d97ec45cb1bbc27db6ea9f5e29
5pence <sbarriball@me.com>
2019-04-18 21:25:59 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char switchVal = 'G';
+	    switch(switchVal) {
+            case 'A':
+                System.out.println("it's A");
+                break;
+            case 'B':
+                System.out.println("it's B");
+                break;
+            case 'C':
+                System.out.println("it's C");
+                break;
+            case 'D':
+                System.out.println("it's D");
+                break;
+            case 'E':
+                System.out.println("it's E");
+                break;
+            default:
+                System.out.println("not found");
+                break;
+
+        }
+
+    }
+}
